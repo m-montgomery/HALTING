8e99e32acbef89999d3a218438f6d48af85bcb2d
M. Montgomery <mmontgomery@protonmail.com>
2018-01-18 20:52:05 -0500

    Initializing project
    Created new Eclipse Java project; built & ran simple test of printing;
    testing Git interface.

---
+++ src/automata/FA.java
@@ -0,0 +1,10 @@
+package automata;
+
+public class FA {
+	public FA() {
+		System.out.println("Created a new FA!");
+	}
+	public void foo() {
+		
+	}
+}
---
+++ src/automata/Testing.java
@@ -0,0 +1,10 @@
+package automata;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		FA f = new FA();
+		f.foo();
+	}
+
+}
